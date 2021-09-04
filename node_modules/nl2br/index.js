dda5ca3277bd5c5742628c69f8d1b0f0d67749f3
vinsdragonis <zrexteam128@gmail.com>
2021-09-04 21:13:59 +0530

    Base server code
    Base server code (Still not tested and maybe unstable)

---
+++ node_modules/nl2br/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = function(str, isXhtml) {
+    var breakTag = (isXhtml || typeof isXhtml === 'undefined') ? '<br />' : '<br>';
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+};
