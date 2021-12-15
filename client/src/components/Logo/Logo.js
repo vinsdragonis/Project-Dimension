1e412b2a24be23c1fef36d8bf5a64febf575b992
vinsdragonis <zrexteam128@gmail.com>
2021-12-15 18:04:21 +0530

    Update Logo.js

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -5,7 +5,11 @@ const Logo = () => {
   return (
     <div className="ma4 mt0">
       <div className="link pa1 pointer grow" style={{ display: "flex", justifyContent: "space-between", marginTop: "7px" }}>
-        <img src={ dragon } style={{ height: "60px" }} alt="Logo" />
+        <img
+          src={ dragon }
+          style={{ height: "60px" }}
+          alt="Logo"
+        />
       </div>
     </div>
   );
